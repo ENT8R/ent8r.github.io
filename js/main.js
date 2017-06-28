9ed68ae1f903b449dfa1b21ddb1f253438904081
L.Lemke <ent8r@users.noreply.github.com>
2017-06-28 18:15:35 +0200

    added new Project called Sweetscanner

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	$(".open-menu-trigger").sideNav();
+	$("#content").load('./projects.html');
+
+	$(".projets-link").click(function(){
+    $("#content").load('./projects.html');
+});
 });
