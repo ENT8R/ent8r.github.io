98841cdc471c42f9c05b2ab868424b583c664712
L.Lemke <ent8r@users.noreply.github.com>
2017-06-29 10:17:58 +0200

    made translation working

--- js/main.js
+++ js/main.js
@@ -4,5 +4,25 @@ $(document).ready(function() {
 
 	$(".projets-link").click(function(){
     $("#content").load('./projects.html');
-});
+	});
+
+	i18next.use(window.i18nextBrowserLanguageDetector).use(window.i18nextXHRBackend).init({
+		debug: true,
+			backend: {
+          loadPath: './locales/{{lng}}/translation.json'
+      }
+	}, function(err, t) {
+		jqueryI18next.init(i18next, $, {
+			tName: 't',
+		  i18nName: 'i18n',
+		  handleName: 'localize',
+		  selectorAttr: 'data-i18n',
+		  targetAttr: 'i18n-target',
+		  optionsAttr: 'i18n-options',
+		  useOptionsAttr: false,
+		  parseDefaultValueFromContent: true
+		});
+		$('#content').localize();
+	});
+
 });
